848bfd6509b611814c7922289bffa3b234481d39
swati006 <69349105+swati006@users.noreply.github.com>
2020-09-10 18:26:31 +0530

    Add files via upload

---
+++ list.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+                                    
+// const  List =()=>{
+//     const arr = [1,2,3,4,5]
+//     console.log(arr[2]);
+
+// return(
+// <ul>
+// <li> {arr[0]}</li>  
+// <li> {arr[1]}</li>  
+// <li> {arr[2]}</li>  
+// <li> {arr[3]}</li>  
+// <li> {arr[4]}</li>  
+
+// </ul>
+// )}
+                                                    //  using map method
+//  const  List =()=>{
+//      const arr =[20,30, 40,50]
+//      const newarr = arr.map((num)=>{
+//     console.log ("NUm=",num )
+//     return<li>{num*2}</li>
+//      })
+//     console.log("oldarr=",arr)
+//     console.log("Newarr=",newarr)
+//  return(
+//         <ul>{newarr}</ul>
+//         )
+//  }                                          
+                                              //using props
+// const List =(props)=>{
+//     const arr = props.numbers
+//     return(
+//     <ul>
+//     {arr.map(num=>(<li>{num}</li>
+//     ))}
+//     </ul>)
+// } 
+   
+           
---
+++ user.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const User=()=>{
+    return(
+    <div>
+         <h1>welcome swati</h1>
+         <button>logout</button>
+    </div>);
+}
+export default User;
